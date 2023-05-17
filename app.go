de578818f9dc4cd2ea4117ddaec14f01fe167fa6
xsobrietyx <vadym.serdiuk@gmail.com>
2023-05-16 22:16:30 -0400

    init

---
+++ app.go
@@ -0,0 +1,14 @@
+package genius_url_shortener
+
+/*
+	TODO:
+		1. REST server with 3 handlers (first is for hashing the string input, second for de-hashing it, third for cleanup purposes)
+		2. internal hashmap that holds hashed as a key and URLs as a value
+		3. pick up proper hashing function
+		4. think about the unit tests
+		5. implementation
+*/
+
+func main() {
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module genius-url-shortener
+
+go 1.18
